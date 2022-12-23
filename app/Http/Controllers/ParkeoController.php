94fe10029f5f9bb6566370444906faf120c343bd
RodrigoJaldin <85416473+RodrigoJaldin@users.noreply.github.com>
2022-12-22 22:14:03 -0400

    migraciones

---
+++ app/Http/Controllers/ParkeoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Parkeo;
+use Illuminate\Http\Request;
+
+class ParkeoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Parkeo  $parkeo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Parkeo $parkeo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Parkeo  $parkeo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Parkeo $parkeo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Parkeo  $parkeo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Parkeo $parkeo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Parkeo  $parkeo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Parkeo $parkeo)
+    {
+        //
+    }
+}
